5911f5ecd0cb2133999dddf48ded3ab01695d2b8
Sean Cherng <seancherng.tw@gmail.com>
2018-03-21 10:50:07 +0800

    Update lab03

--- Natural-Language-Processing-Lab/Lab03_noisy_channel/lab3_noisy_channel.py
+++ Natural-Language-Processing-Lab/Lab03_noisy_channel/lab3_noisy_channel.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 import re
 import math
-from collections import Counter, defaultdict
 from pprint import pprint
+from collections import Counter, defaultdict
 '''Word Probability'''
 
 
@@ -25,14 +25,16 @@ count_c = defaultdict(lambda: 0)
 for line in open('count_1edit.txt'):
     edit, count = line.split('\t')[0], int(line.split('\t')[1].replace('\n', ''))
     w, c = edit.split('|')[0], edit.split('|')[1]
-    count_1edit[(w, c)] += count
-    count_c[c] += 1
+    count_1edit[(w, c)] = count
+    count_c[c] += count
 
 r = 10
 N = dict()
-for i in range(1, r):
+for i in range(1, r + 2):
     N[i] = (sum(count for count in count_1edit.values() if count == i)) // i
+
 N[0] = 26 * 26 * 26 * 26 + 2 * 26 * 26 * 26 + 26 * 26 - sum(N.values())
+Nall = len(count_1edit.keys())
 
 
 def smooth(count, r=10):
@@ -42,18 +44,27 @@ def smooth(count, r=10):
         return count
 
 
+smooth(0)
+Nall
+count_c['e']
+
+
 def Pedit(w, c):
-    if count_c[c] > 0:
-        return smooth(count_c[c]) / count_c[c]
+    if (w, c) in count_1edit:
+        return smooth(count_1edit[w, c]) / count_c[c]
     else:
-        return 0
+        if c in count_c:
+            return smooth(0) / count_c[c]
+        else:
+            return 10**(-20)
 
 
+Pedit("e", "i")
 '''Combining channel probability with word probability to score states'''
 
 
 def P(pedit, pw):
-    return (pedit * pw) * 10 ** 7
+    return math.log(pedit) + math.log(pw)
 
 
 '''Next States'''
@@ -61,47 +72,50 @@ letters = 'abcdefghijklmnopqrstuvwxyz'
 
 
 def next_states(state):
-    L, R, edits, pw, pedit = state  # (str, str, list, float, float)
+    L, R, edits, prob, pedit = state
     R0, R1 = R[0], R[1:]
-    if edits == 2:
-        return [(L + R0, R1, edits, pw, pedit * 0.8)]
-    noedit = [(L + R0, R1, edits, pw, pedit * 0.8)]
-    if len(L) > 0:
-        delete = [(L, R1, edits + 1, Pw(L + R1), P(Pedit(L[-1], L[-1] + R0), Pw(L + R1)))]
-    else:
-        delete = [(L, R1, edits + 1, Pw(L + R1), P(Pedit('', R0), Pw(L + R1)))]
-    insert = [(L + R0 + c, R1, edits + 1, Pw(L + R0 + c + R1), P(Pedit(R0, R0 + c), Pw(L + R0 + c + R1))) for c in letters]
-    replace = [(L + c, R1, edits + 1, Pw(L + c + R1), P(Pedit(R0, R1), Pw(L + c + R1))) for c in letters]
-    if len(R1) > 0:
-        transpose = [(L + R1[0], R0 + R1[1:], edits + 1, Pw(L + R1[0] + R0 + R1[1:]),
-                      P(Pedit(R0 + R1[0], R1[0] + R0), Pw(L + R1[0] + R0 + R1[1:])))]
-    else:
-        transpose = []
+
+    if len(edits) == 2:
+        return [(L + R0, R1, edits, prob, pedit * 0.8)]
+
+    noedit = [(L + R0, R1, edits, prob, pedit * 0.8)]
+    delete = [(L, R1, edits + [(L[-1:] + R0, L[-1:])], Pw(L + R1), pedit * Pedit(L[-1:] + R0, L[-1:]))]
+    replace = [(L + c, R1, edits + [(R0, c)], Pw(L + c + R1), pedit * Pedit(R0, c)) for c in letters]
+    insert = [(L + R0 + c, R1, edits + [(R0, R0 + c)], Pw(L + R0 + c + R1), pedit * Pedit(R0, R0 + c)) for c in letters]
+    transpose = [(L + R1[0], R0 + R1[1:], edits + [(R0 + R1[0], R1[0] + R0)], Pw(L + R1[0] +
+                                                                                 R0 + R1[1:]), pedit * Pedit(R0 + R1[0], R1[0] + R0))] if len(R1) > 0 else []
+
     return noedit + delete + insert + replace + transpose
-
-
-'''Correcting'''
-MAXBEAM = 1000
+# Using Pw(word) or P(word) may result in different answers
 
 
 def correction(word):
-    states = [('', word, 0, Pw(word), 1)]  # initial state
+    states = [("", word, [], Pw(word), 1)]
+    MAXBEAM = 550
+
     for i in range(len(word)):
-        states = [newstates for state in states for newstates in next_states(state)]
-        states = [state for state in states if state[4] > 0]
+        states = [state for states in map(next_states, states) for state in states]
 
-        temp = defaultdict(list)
+        word_dict = {}
         for state in states:
-            L, R, edits, pw, pedit = state
-            temp[L + R].append(state)
-        states = [min(substates, key=lambda x: x[2]) for wd, substates in temp.items()]
+            L, R, edits, prob, pedit = state
+            word = L + R
+            if word not in word_dict or len(edits) < len(word_dict[word][2]):
+                word_dict[word] = state
 
-        states = sorted(states, key=lambda x: x[4], reverse=True)
-        states = sorted(states, key=lambda x: x[2])[:MAXBEAM]
+        states = list(word_dict.values())
+        states = sorted(states, key=lambda x: P(x[3], x[4]), reverse=True)
+        states = sorted(states, key=lambda x: len(x[2]))[:MAXBEAM]
 
-    states = [state for state in states if state[4] > 0]
+    states = sorted(states, key=lambda x: P(x[3], x[4]), reverse=True)
+    return states[:3]
 
-    return sorted(states, key=lambda x: x[4], reverse=True)[:3]
 
-
-correction("appearant")
+correction('appearant')
+correction("runing")
+correction("particpate")
+correction("beleive")
+correction('writtung')
+correction('happy')
+correction('thenks')
+get_ipython().magic('save lab3_noisy_channel.py 1-20')
