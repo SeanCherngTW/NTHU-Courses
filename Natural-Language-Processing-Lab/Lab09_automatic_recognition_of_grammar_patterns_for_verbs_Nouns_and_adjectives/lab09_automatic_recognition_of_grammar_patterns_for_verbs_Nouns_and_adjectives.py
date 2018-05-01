3a1ea891247262314e3f36fd3f2c4ead5360b0b2
Sean Cherng <seancherng.tw@gmail.com>
2018-05-01 17:10:44 +0800

    Update test case

--- Natural-Language-Processing-Lab/Lab09_automatic_recognition_of_grammar_patterns_for_verbs_Nouns_and_adjectives/lab09_automatic_recognition_of_grammar_patterns_for_verbs_Nouns_and_adjectives.py
+++ Natural-Language-Processing-Lab/Lab09_automatic_recognition_of_grammar_patterns_for_verbs_Nouns_and_adjectives/lab09_automatic_recognition_of_grammar_patterns_for_verbs_Nouns_and_adjectives.py
@@ -8,8 +8,6 @@ akl = list(akl.akl.keys())
 PRONS = set([line.strip('\n') for line in open('prons.txt')])
 with open('HiFreWords') as f:
     HiFreWords = set(f.readline().split('\t'))
-
-
 def create_sentence_pattern_list(input_pat):
     pattern = []
     final = []
@@ -23,13 +21,9 @@ def create_sentence_pattern_list(input_pat):
     # Last one
     final.append(pattern)
     return final
-
-
 # Corpus
 corpus = open('corpus_all.txt', 'r').read().strip('\n').split('\n')
 corpus = create_sentence_pattern_list(corpus)
-
-
 def build_pattern_dict():
     pattern_dict = defaultdict(lambda: defaultdict(list))
     example_sentences = defaultdict(lambda: defaultdict(list))
@@ -39,29 +33,25 @@ def build_pattern_dict():
             term, grammar, pattern = c.split('\t')
             pattern_dict[term][grammar] += [pattern]
             example_sentences[term][grammar] += [sent.split()]
-
+            
     return pattern_dict, example_sentences
-
-
 pattern_dict, example_sentences = build_pattern_dict()
-
-
 def check_pattern_dict():
     print('ABILITY -N')
     print('N to v \t\t(pd:%d, label:468)\n' % len(pattern_dict['ABILITY']['N to v']))
-
+    
     print('VALUE -N')
     print('N to v \t\t(pd:%3d, label: 16)\n' % len(pattern_dict['VALUE']['N to v']))
-
+    
     print('DISCUSS -V')
     print('V in n \t\t(pd:%3d, label: 47)' % len(pattern_dict['DISCUSS']['V in n']))
     print('V n \t\t(pd:%3d, label:270)' % len(pattern_dict['DISCUSS']['V n']))
     print('V wh to v \t(pd:%3d, label: 15)\n' % len(pattern_dict['DISCUSS']['V wh to v']))
-
+    
     print('FAVOUR -V')
     print('V n \t\t(pd:%3d, label: 26)' % len(pattern_dict['FAVOUR']['V n']))
     print('V by n \t\t(pd:%3d, label:  5)\n' % len(pattern_dict['FAVOUR']['V by n']))
-
+    
     print('CLASSIFY -V')
     print('V into n \t(pd:%3d, label:  8)' % len(pattern_dict['CLASSIFY']['V into n']))
     print('V as n \t\t(pd:%3d, label: 12)\n' % len(pattern_dict['CLASSIFY']['V as n']))
@@ -69,41 +59,35 @@ def check_pattern_dict():
     print('USEFUL -ADJ')
     print('ADJ to v \t(pd:%3d, label: 30)' % len(pattern_dict['USEFUL']['ADJ to v']))
     print('ADJ for n \t(pd:%3d, label: 20)\n' % len(pattern_dict['USEFUL']['ADJ for n']))
-
+    
     print('CERTAIN -ADJ')
     print('ADJ of n \t(pd:%3d, label: 23)' % len(pattern_dict['CERTAIN']['ADJ of n']))
-
-
 check_pattern_dict()
-
-
 def computeScore(word, sent):
     global PRONS
     global HiFreWords
-
+    
     word = word.lower()
     sent = sent.lower().split()
     length = len(sent)
-
-    locationOfWord = -1 if word not in sent else sent.index(word)
+    
+    locationOfWord = -1 if word not in sent else sent.index(word) 
     hiFreWordsScore = len([w for w in sent if w not in HiFreWords])
     pronsScore = len([w for w in sent if w in PRONS])
-
+    
     return locationOfWord - hiFreWordsScore - pronsScore
-
-
 def get_best_pattern(word):
     avg = 0.0
     stddev = 0.0
     k0 = 1
-
+    
     word = word.upper()
-
+    
     print(word)
 
     # Total grammar count for the input word
     N = len(pattern_dict[word].keys())
-
+    
     if N == 0:
         print('NO RESULT\n')
         return
@@ -119,14 +103,14 @@ def get_best_pattern(word):
         freqi = len(sentences)
         stddev += (freqi - avg) ** 2
     stddev = math.sqrt(stddev / N - 1)
-
+        
     if stddev == 0:
         print('NO RESULT\n')
         return
 
     best_score = -999.9
     best_sentence = ''
-
+    
     # Filter good grammar
     for grammar, sentences in pattern_dict[word].items():
         freqi = len(sentences)
@@ -143,8 +127,6 @@ def get_best_pattern(word):
 
         print('%s (%d) %s' % (grammar, freqi, best_sentence))
     print()
-
-
 def test_case():
     get_best_pattern('ability')
     get_best_pattern('value')
@@ -153,6 +135,6 @@ def test_case():
     get_best_pattern('classify')
     get_best_pattern('useful')
     get_best_pattern('certain')
-
-
+    get_best_pattern('remain')
+    get_best_pattern('allow')
 test_case()
